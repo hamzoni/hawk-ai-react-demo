dec8d65baaecef77ef46b8c3fbbe520f0f8e0cd8
TA QUY <taquy.se@gmail.com>
2020-10-01 15:57:07 +0700

    fix: missing config for ws and kafka messaging

--- src/App.js
+++ src/App.js
@@ -7,38 +7,41 @@ import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import {QueueEvents} from "./constants/QueueEvents";
+import {WebSocketRoot} from "./constants/WsConstants";
 
-const SOCKET_URL = 'http://localhost:9000/ws-chat/';
+const SOCKET_URL = `http://localhost:9000/${WebSocketRoot}/`;
 
 const App = () => {
     let onConnected = () => {
         console.log("Connected!!")
     };
 
-    let onMessageReceived = (msg) => {
-        console.log('New Message Received!!', msg);
-        // setMessages(messages.concat(msg));
+    let onMessageReceived = (msg, name) => {
+        console.log(name, msg);
     };
 
     let onSendMessage = (msgText) => {
         console.log(msgText);
     };
 
-    const [value, setValue] = React.useState(2);
+    const [value, setValue] = useState(QueueEvents.PUT_TRANSACTION);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
+
     return (
         <div className="App">
-            <SockJsClient
+            {Object.values(QueueEvents).map(name => <SockJsClient
                 url={SOCKET_URL}
-                topics={['/topic/group']}
+                topics={[`/${WebSocketRoot}/${name}`]}
                 onConnect={onConnected}
                 onDisconnect={console.log("Disconnected!")}
-                onMessage={msg => onMessageReceived(msg)}
+                onMessage={msg => onMessageReceived(msg, name)}
                 debug={false}
-            />
+            />)}
+
             <Container maxWidth="lg" component={'div'}>
                 <Paper square>
                     <Tabs
@@ -50,8 +53,8 @@ const App = () => {
                         component={'div'}
                         variant="fullWidth"
                     >
-                        <Tab label="Active" href={''} value={}/>
-                        <Tab label="Active" href={''}/>
+                        <Tab label="Transactions" href={''} value={QueueEvents.PUT_TRANSACTION}/>
+                        <Tab label="Dormant activations" href={''} value={QueueEvents.ACTIVATE_DORMANT}/>
                     </Tabs>
                 </Paper>
 
---
+++ src/constants/QueueEvents.ts
@@ -0,0 +1,4 @@
+export enum QueueEvents {
+    PUT_TRANSACTION = "new_transaction",
+    ACTIVATE_DORMANT = "activate_dormant_account",
+}
